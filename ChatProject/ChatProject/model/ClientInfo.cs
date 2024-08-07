f34daeba3873f1d34164b7ecc4dd10158bfb5895
tri-huynh-th <2251120119@ut.edu.vn>
2024-08-07 10:38:22 +0700

    trien khai

---
+++ ChatProject/ChatProject/model/ClientInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatProject.model
+{
+    internal class ClientInfo
+    {
+
+        public string name { get; set; }
+        public TcpClient tpc { get; set; }
+
+    }
+}
--- ChatProject/Client/ChatForm.cs
+++ ChatProject/Client/ChatForm.cs
@@ -1,23 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Client
 {
     public partial class ChatForm : Form
     {
+        private string userName;
+        private string severIp;
+        private TcpClient client;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private Thread clientThread;
+
         public ChatForm()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        public ChatForm(string userName, string severIp)
+        {
+            InitializeComponent();
+
+            this.userName = userName;
+            this.severIp = severIp;
+
+            userNamelabel.Text = userName;
+            ipLabel.Text =  severIp;
+
+            connectToServer();
+        }
+
+        private void connectToServer()
+        {
+            try
+            {
+                int port = 9000;
+                client = new TcpClient(severIp, port);
+
+                var stream = client.GetStream();
+                reader = new StreamReader(stream, Encoding.UTF8);
+                writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+                writer.WriteLine(userName);
+
+                clientThread = new Thread(receiveMessages);
+                clientThread.Start();
+
+                appendMessage("Kết nối thành công đến server.");
+            }
+            catch (Exception ex)
+            {
+                appendMessage("Không thể kết nối đến server: " + ex.Message);
+            }
+        }
+
+        private void receiveMessages()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    var message = reader.ReadLine();
+                    if (message != null)
+                    {
+                        appendMessage(message);
+                    }
+                    else
+                    {
+                        appendMessage("Server đã ngắt kết nối.");
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    appendMessage("Mất kết nối đến server: " + ex.Message);
+                    break;
+                }
+            }
+        }
+
+        private void appendMessage(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(appendMessage), message);
+                return;
+            }
+            messageBox.AppendText(message + Environment.NewLine);
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            var message = sendBox.Text.Trim();
+            if (!string.IsNullOrEmpty(message))
+            {
+                writer.WriteLine(message);
+                appendMessage("Bạn: "+ message);
+                sendBox.Clear();
+            }
+        }
+
+        private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                client.Close();
+            }
+            if (clientThread != null && clientThread.IsAlive)
+            {
+                clientThread.Abort();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendBox_TextChanged(object sender, EventArgs e)
         {
 
         }
